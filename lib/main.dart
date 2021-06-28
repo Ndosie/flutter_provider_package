6ccaf34669adb1e432a182a6123d21dba66a02e4
Esther Ndosi <estherndosi24@gmail.com>
2021-06-28 12:18:39 +0300

    Editing Start of Provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,22 @@ import 'homepage.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (_) => MyCart(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MyCart>(
-      create: (_) => MyCart(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-        ),
-        home: HomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
       ),
+      home: HomePage(),
     );
   }
 }
